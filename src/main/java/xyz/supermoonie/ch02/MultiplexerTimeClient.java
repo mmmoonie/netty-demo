0a0bb4b269017c2cddb50261cc3801d368ba04fc
moonie <wangchao_moonie@163.com>
2018-02-23 17:04:13 +0800

    MultiplexerTimeClient

---
+++ src/main/java/xyz/supermoonie/ch02/MultiplexerTimeClient.java
@@ -0,0 +1,150 @@
+package xyz.supermoonie.ch02;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author moonie
+ * @date 2018/2/23
+ */
+public class MultiplexerTimeClient {
+
+    public static void main(String[] args) {
+        int port = 7100;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        new Thread(new TimeClientHandle(null, port)).start();
+    }
+}
+
+class TimeClientHandle implements Runnable {
+
+    private String host;
+
+    private int port;
+
+    private Selector selector;
+
+    private SocketChannel socketChannel;
+
+    private volatile boolean stop = false;
+
+    public TimeClientHandle(String host, int port) {
+        this.host = host == null ? "127.0.0.1" : host;
+        this.port = port;
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            selector = Selector.open();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            doConnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (!stop) {
+            try {
+                selector.select(10000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectionKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            if (key.isConnectable()) {
+                if (socketChannel.finishConnect()) {
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                    doWrite(socketChannel);
+                } else {
+                    // 连接失败，进程退出
+                    System.exit(1);
+                }
+            }
+            if (key.isReadable()) {
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = socketChannel.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("now is: " + body);
+                    this.stop = true;
+                } else if (readBytes < 0) {
+                    // 对端链路关闭
+                    key.cancel();
+                    socketChannel.close();
+                } else {
+                    ; // 读到 0 字节，忽略
+                }
+            }
+        }
+    }
+
+    private void doConnect() throws IOException {
+        if (socketChannel.connect(new InetSocketAddress(this.host, port))) {
+            socketChannel.register(selector, SelectionKey.OP_READ);
+            doWrite(socketChannel);
+        } else {
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
+        }
+    }
+
+    private void doWrite(SocketChannel socketChannel) throws IOException {
+        byte[] bytes = "QUERY TIME ORDER".getBytes();
+        ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+        writeBuffer.put(bytes);
+        writeBuffer.flip();
+        socketChannel.write(writeBuffer);
+        if (!writeBuffer.hasRemaining()) {
+            System.out.println("send order 2 server succeed");
+        }
+    }
+}
