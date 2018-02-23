b2837a3dc9d94a2ee0508720a3adfe1bbb6687ee
moonie <wangchao_moonie@163.com>
2018-02-23 15:56:06 +0800

    MultiplexerTimeServer

---
+++ src/main/java/xyz/supermoonie/ch02/MultiplexerTimeServer.java
@@ -0,0 +1,140 @@
+package xyz.supermoonie.ch02;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author moonie
+ * @date 2018/2/23
+ */
+public class MultiplexerTimeServer {
+
+    public static void main(String[] args) {
+        int port = 7100;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        MultiplexerTimeServerThread multiplexerTimeServerThread = new MultiplexerTimeServerThread(port);
+        new Thread(multiplexerTimeServerThread, "nio-MultiplexerTimeServerThread-001").start();
+    }
+}
+
+class MultiplexerTimeServerThread implements Runnable {
+
+    private Selector selector;
+
+    private ServerSocketChannel socketChannel;
+
+    private volatile boolean stop = false;
+
+    public MultiplexerTimeServerThread(int port) {
+        try {
+            selector = Selector.open();
+            socketChannel = ServerSocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.socket().bind(new InetSocketAddress(port), 1024);
+            socketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("the time server is start in port: " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectionKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    System.out.println("yes yes yes");
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                    }
+                }
+                System.out.println("no no no");
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            if (key.isAcceptable()) {
+                System.out.println("acceptable....");
+                ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                SocketChannel socketChannel = serverSocketChannel.accept();
+                socketChannel.configureBlocking(false);
+                socketChannel.register(selector, SelectionKey.OP_READ);
+            }
+            if (key.isReadable()) {
+                System.out.println("readable.....");
+                SocketChannel socketChannel = (SocketChannel) key.channel();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = socketChannel.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("the time server receive order: " + body);
+                    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+                    doWrite(socketChannel, currentTime);
+                } else if (readBytes < 0) {
+                    System.out.println("对端链路关闭");
+                    // 对端链路关闭
+                    key.cancel();
+                    socketChannel.close();
+                } else {
+                    // 读到 0 字节，忽略
+                    System.out.println("读到 0 字节，忽略");
+                }
+            }
+        }
+    }
+
+    private void doWrite(SocketChannel socketChannel, String response) throws IOException {
+        if (response != null && response.trim().length() > 0) {
+            byte[] bytes = response.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            socketChannel.write(writeBuffer);
+        }
+    }
+}
