7a211d0fdb91a8ba86f222a2fa2874930771c82d
moonie <wangchao_moonie@163.com>
2018-02-25 12:09:07 +0800

    AsyncTimeServer AsyncTimeClient

---
+++ src/main/java/xyz/supermoonie/ch02/AsyncTimeServer.java
@@ -0,0 +1,144 @@
+package xyz.supermoonie.ch02;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author moonie
+ * @date 2018/2/25
+ */
+public class AsyncTimeServer {
+
+    public static void main(String[] args) {
+        int port = 7100;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        AsyncTimeServerHandler asyncTimeServerHandler = new AsyncTimeServerHandler(port);
+        new Thread(asyncTimeServerHandler, "AIO-AsyncTimeServerHandler-001").start();
+    }
+}
+
+class AsyncTimeServerHandler implements Runnable{
+
+    private int port;
+
+    CountDownLatch latch;
+
+    AsynchronousServerSocketChannel asynchronousServerSocketChannel;
+
+    public AsyncTimeServerHandler(int port) {
+        this.port = port;
+        try {
+            asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open();
+            asynchronousServerSocketChannel.bind(new InetSocketAddress(this.port));
+            System.out.println("the time server is start in port: " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        latch = new CountDownLatch(1);
+        doAccept();
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void doAccept() {
+        asynchronousServerSocketChannel.accept(this, new AcceptCompletionHandler());
+    }
+}
+
+class AcceptCompletionHandler implements CompletionHandler<AsynchronousSocketChannel, AsyncTimeServerHandler>{
+
+
+    @Override
+    public void completed(AsynchronousSocketChannel result, AsyncTimeServerHandler attachment) {
+        attachment.asynchronousServerSocketChannel.accept(attachment, this);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        result.read(buffer, buffer, new ReadCompletionHandler(result));
+    }
+
+    @Override
+    public void failed(Throwable exc, AsyncTimeServerHandler attachment) {
+        exc.printStackTrace();
+        attachment.latch.countDown();
+    }
+}
+
+class ReadCompletionHandler implements CompletionHandler<Integer, ByteBuffer> {
+
+    private AsynchronousSocketChannel asynchronousSocketChannel;
+
+    public ReadCompletionHandler(AsynchronousSocketChannel asynchronousSocketChannel) {
+        if (asynchronousSocketChannel != null) {
+            this.asynchronousSocketChannel = asynchronousSocketChannel;
+        }
+    }
+
+    @Override
+    public void completed(Integer result, ByteBuffer attachment) {
+        attachment.flip();
+        byte[] body = new byte[attachment.remaining()];
+        attachment.get(body);
+        try {
+            String req = new String(body, "UTF-8");
+            System.out.println("the time server receive order: " + req);
+            String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(req) ? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+            doWrite(currentTime);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void failed(Throwable exc, ByteBuffer attachment) {
+        try {
+            asynchronousSocketChannel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doWrite(String currentTime) {
+        if (currentTime != null && currentTime.trim().length() > 0) {
+            byte[] bytes = currentTime.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            asynchronousSocketChannel.write(writeBuffer, writeBuffer,
+                    new CompletionHandler<Integer, ByteBuffer>() {
+                        @Override
+                        public void completed(Integer result, ByteBuffer attachment) {
+                            if (attachment.hasRemaining()) {
+                                asynchronousSocketChannel.write(writeBuffer, writeBuffer, this);
+                            }
+                        }
+
+                        @Override
+                        public void failed(Throwable exc, ByteBuffer attachment) {
+                            try {
+                                asynchronousSocketChannel.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+        }
+    }
+}
