5e1ce85e0f34d5d13f1cc6789f6ac21cf98bcc38
moonie <wangchao_moonie@163.com>
2018-02-28 13:44:39 +0800

    UserInfo

---
+++ src/main/java/xyz/supermoonie/ch06/TestUserInfo.java
@@ -0,0 +1,27 @@
+package xyz.supermoonie.ch06;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * @author moonie
+ * @date 2018/2/28
+ */
+public class TestUserInfo {
+
+    public static void main(String[] args) throws IOException {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserId(10);
+        userInfo.setUserName("netty");
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(bos);
+        os.writeObject(userInfo);
+        os.flush();
+        os.close();
+        byte[] b = bos.toByteArray();
+        System.out.println("java jdk serializable length: " + b.length);
+        bos.close();
+        System.out.println("byte array serializable length: " + userInfo.encode().length);
+    }
+}
---
+++ src/main/java/xyz/supermoonie/ch06/UserInfo.java
@@ -0,0 +1,45 @@
+package xyz.supermoonie.ch06;
+
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+
+/**
+ * @author moonie
+ * @date 2018/2/28
+ */
+public class UserInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String userName;
+
+    private int userId;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public byte[] encode() {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        byte[] value = this.userName.getBytes();
+        buffer.putInt(value.length);
+        buffer.put(value);
+        buffer.putInt(this.userId);
+        buffer.flip();
+        byte[] result = new byte[buffer.remaining()];
+        buffer.get(result);
+        return result;
+    }
+}
